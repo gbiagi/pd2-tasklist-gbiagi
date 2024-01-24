70d7e9e8f60cc1f8977aa547a7abc084e0642d44
geanfrancobiagi <geanfrancobiagi@hotmail.com>
2024-01-24 21:03:58 +0100

    Edit+delete OK

--- www/js/index.js
+++ www/js/index.js
@@ -9,11 +9,33 @@ function onDeviceReady() {
 }
 
 function addTask() {
-    let task = prompt("Introduce tu tarea");
+    var task = prompt("Por favor, introduce tu tarea");
     if (task != null) {
-        $('ul[data-role="listview"]').append('<li>' + task + '<button class="editButton">Edit</button><button class="deleteButton">X</button></li>').listview('refresh');
+        var newTaskItem = $('<li data-icon="false"><div class="list-item-container"><span class="taskText">' + task + '</span><div><button class="editButton">Edit</button><button class="deleteButton">X</button></div></div></li>');
+        $('ul[data-role="listview"]').append(newTaskItem).listview('refresh');
+
+        newTaskItem.find('.editButton').click(editTask);
+        newTaskItem.find('.deleteButton').click(deleteTask);
     }
 }
+
+function editTask() {
+    var taskItem = $(this).closest('li');
+    var taskText = taskItem.find('.taskText').text();
+    taskItem.find('.taskText').html('<input type="text" class="editInput" value="' + taskText + '"><button class="confirmEditButton">OK</button>');
+
+    taskItem.find('.confirmEditButton').click(function () {
+        var newTask = taskItem.find('.editInput').val();
+        if (newTask != null) {
+            taskItem.find('.taskText').text(newTask);
+        }
+    });
+}
+
+function deleteTask() {
+    $(this).closest('li').remove();
+}
+
 $(document).ready(function () {
     // initialize
     $('#buttonAddtask').click(addTask);
