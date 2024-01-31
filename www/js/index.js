767832811b4d61db5549b8cbdd36d1a50da73b69
geanfrancobiagi <geanfrancobiagi@hotmail.com>
2024-01-31 20:25:09 +0100

    localstorage Ok

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,9 @@ function addTask() {
 
         newTaskItem.find('.editButton').click(editTask);
         newTaskItem.find('.deleteButton').click(deleteTask);
+        saveTasks();
+        // Save task in localStorage
+
     }
 }
 
@@ -28,16 +31,42 @@ function editTask() {
         var newTask = taskItem.find('.editInput').val();
         if (newTask != null) {
             taskItem.find('.taskText').text(newTask);
+            saveTasks();
         }
     });
 }
 
 function deleteTask() {
     $(this).closest('li').remove();
+    saveTasks();
+}
+
+function saveTasks() {
+    // Extract task text from each list item and store them in an array
+    var tasks = $('ul[data-role="listview"] li .taskText').map(function () {
+        return $(this).text();
+    }).get();
+
+    // Save the tasks array to localStorage
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function restoreTasks() {
+    var tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+    // Create list items for each task and append them to the task list
+    for (var i = 0; i < tasks.length; i++) {
+        var newTaskItem = $('<li data-icon="false"><div class="list-item-container"><span class="taskText">' + tasks[i] + '</span><div><button class="editButton">Edit</button><button class="deleteButton">X</button></div></div></li>');
+        $('ul[data-role="listview"]').append(newTaskItem).listview('refresh');
+
+        newTaskItem.find('.editButton').click(editTask);
+        newTaskItem.find('.deleteButton').click(deleteTask);
+    }
 }
 
 $(document).ready(function () {
     // initialize
+    restoreTasks();
     $('#buttonAddtask').click(addTask);
 });
 
